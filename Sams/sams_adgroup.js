0cb9f6fb0de5c72d74a4d228ba8c007b99c4bf9b
YuI2333 <yuilk@qq.com>
2026-08-06 05:48:33 +0800

    更新 sams_adgroup.js

--- Sams/sams_adgroup.js
+++ Sams/sams_adgroup.js
@@ -4,10 +4,16 @@ try {
     if (obj && obj.data) {
         if (Array.isArray(obj.data)) {
             obj.data = [];
-        } else typeof obj.data === 'object' {
+        } else if (typeof obj.data === 'object') {
             for (let key in obj.data) {
                 if (Array.isArray(obj.data[key])) {
                     obj.data[key] = [];
+                } else if (typeof obj.data[key] === 'object' && obj.data[key] !== null) {
+                    for (let subKey in obj.data[key]) {
+                        if (Array.isArray(obj.data[key][subKey])) {
+                            obj.data[key][subKey] = [];
+                        }
+                    }
                 }
             }
         }
