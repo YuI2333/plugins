b4fb4ee92704be3c0b43e2587d2124b2137ac5f4
YuI2333 <yuilk@qq.com>
2026-08-07 03:46:45 +0800

    更新 zhihu_search_hot.js

--- zhihu/zhihu_search_hot.js
+++ zhihu/zhihu_search_hot.js
@@ -1,7 +1,10 @@
 try {
     let obj = JSON.parse($response.body);
     
-    if (obj.recommend_queries) obj.recommend_queries.queries = [];
+    // 清空搜索框下方的热词列表数据源
+    if (obj.recommend_queries) {
+        obj.recommend_queries.queries = [];
+    }
     
     $done({ body: JSON.stringify(obj) });
 } catch (e) {
