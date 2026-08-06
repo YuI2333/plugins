974657cc34a386cb4e1da7af10cd8cac12db3767
YuI2333 <yuilk@qq.com>
2026-08-07 02:49:35 +0800

    更新 xhh_sign_popup.js

--- XHH/xhh_sign_popup.js
+++ XHH/xhh_sign_popup.js
@@ -1,16 +1,16 @@
 try {
     let obj = JSON.parse($response.body);
-    
-    delete obj.msg;
-    
-    if (obj.result) {
-        delete obj.result.notify_description;
-        delete obj.result.description;
-        delete obj.result.sign_in_coin;
-        delete obj.result.sign_in_exp;
-        delete obj.result.level_info;
+
+    if ($request.url.includes("get_sign_state")) {
+        delete obj.msg;
+        if (obj.result) {
+            ["notify_description", "description", "sign_in_coin", "sign_in_exp", "level_info"].forEach(k => delete obj.result[k]);
+        }
+    } else if (obj.result) {
+        // 如果不是签到接口，必然是弹窗接口，直接清空列表
+        obj.result.popup_list = [];
     }
-    
+
     $done({ body: JSON.stringify(obj) });
 } catch (e) {
     $done({});
