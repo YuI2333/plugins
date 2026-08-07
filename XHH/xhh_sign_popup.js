641dbcb0fa2740cdd0eef3822ae11e7549ff4699
YuI2333 <yuilk@qq.com>
2026-08-08 00:25:03 +0800

    更新 xhh_sign_popup.js

--- XHH/xhh_sign_popup.js
+++ XHH/xhh_sign_popup.js
@@ -3,8 +3,13 @@ try {
     let url = $request.url;
 
     if (url.includes("get_sign")) {
-        delete obj.msg;
+        // 赋值为空字符串更安全，防止前端解析报错
+        obj.msg = ""; 
         if (obj.result) {
+            // 将触发弹窗的布尔值强制拦截为 false
+            obj.result.plus_hcoin = false;
+            obj.result.plus_exp = false;
+            // 清理其他显示字段
             ["notify_description", "description", "sign_in_coin", "sign_in_exp", "level_info"].forEach(k => delete obj.result[k]);
         }
     } else if (obj.result) {
