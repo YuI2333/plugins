3456fe6b7148d4f877ae833c9cee8a160346022e
YuI2333 <yuilk@qq.com>
2026-08-07 18:50:17 +0800

    更新 sams_store_visit.js

--- Sams/sams_store_visit.js
+++ Sams/sams_store_visit.js
@@ -2,13 +2,8 @@ let body = $response.body;
 try {
     let obj = JSON.parse(body);
     if (obj.data && Array.isArray(obj.data.categoryList)) {
-        // 定义需要屏蔽的分类按钮名称黑名单
-        const blockList = ["门店商品", "夏日好物"];
-        
-        // 过滤掉黑名单中的分类
-        obj.data.categoryList = obj.data.categoryList.filter(
-            item => !blockList.includes(item.title)
-        );
+        // 一刀切：直接清空分类列表，无视任何现有或未来新增的关键词
+        obj.data.categoryList = [];
     }
     $done({ body: JSON.stringify(obj) });
 } catch (e) {
