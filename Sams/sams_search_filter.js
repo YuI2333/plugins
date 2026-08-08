e7dc8c3fceccd55909e79f3ee08b7cd66bf2775d
YuI2333 <yuilk@qq.com>
2026-08-09 05:07:32 +0800

    更新 sams_search_filter.js

--- Sams/sams_search_filter.js
+++ Sams/sams_search_filter.js
@@ -1,18 +1,21 @@
-try {
-    let body = JSON.parse($response.body);
-    
-    if (body?.data) {
-        body.data.dataList = [];
-        body.data.searchFilterList = [];
-        body.data.cardFilterList = [];
-        body.data.totalCount = 0;
-        body.data.pageSize = 0;
-        body.data.hasNextPage = false;
-        body.data.isNextPage = false;
-    }
-    
-    $done({ body: JSON.stringify(body) });
-} catch (e) {
-    console.log("Sams search filter error: " + e);
+if (typeof $response === "undefined" || !$response.body) {
     $done({});
+} else {
+    try {
+        let body = JSON.parse($response.body);
+        
+        if (body?.data) {
+            body.data.dataList = [];
+            body.data.searchFilterList = [];
+            body.data.cardFilterList = [];
+            body.data.totalCount = 0;
+            body.data.pageSize = 0;
+            body.data.hasNextPage = false;
+            body.data.isNextPage = false;
+        }
+        
+        $done({ body: JSON.stringify(body) });
+    } catch (e) {
+        $done({});
+    }
 }
