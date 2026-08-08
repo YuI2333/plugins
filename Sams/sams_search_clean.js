4e2996666783566811bd0acbb52c6caee3c9c585
YuI2333 <yuilk@qq.com>
2026-08-09 02:37:45 +0800

    更新 sams_search_clean.js

--- Sams/sams_search_clean.js
+++ Sams/sams_search_clean.js
@@ -1,24 +1,27 @@
 if (typeof $response === "undefined" || !$response.body) {
     $done({});
-}
+} else {
+    try {
+        let obj = JSON.parse($response.body);
+        let url = $request.url;
 
-try {
-    let obj = JSON.parse($response.body);
-    let url = $request.url;
-
-    if (obj && obj.data) {
-        if (url.includes("searchRecommendByKeyword")) {
-            if (Array.isArray(obj.data.dataList)) {
-                obj.data.dataList = [];
-            }
-        } else if (url.includes("/spu/search")) {
-            if (obj.data.trySearchKeywordResponse) {
-                delete obj.data.trySearchKeywordResponse;
+        if (obj && obj.data) {
+            // 处理“为您推荐”
+            if (url.includes("searchRecommendByKeyword")) {
+                if (obj.data.dataList && Array.isArray(obj.data.dataList)) {
+                    obj.data.dataList = [];
+                }
+            } 
+            // 处理“试试搜这些”
+            else if (url.includes("/spu/search")) {
+                if (obj.data.trySearchKeywordResponse && Array.isArray(obj.data.trySearchKeywordResponse.dataList)) {
+                    obj.data.trySearchKeywordResponse.dataList = [];
+                }
             }
         }
+        
+        $done({ body: JSON.stringify(obj) });
+    } catch (e) {
+        $done({});
     }
-    
-    $done({ body: JSON.stringify(obj) });
-} catch (e) {
-    $done({});
 }
