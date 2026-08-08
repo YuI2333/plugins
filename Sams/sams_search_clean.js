eed7b4b2a4a2e7e9d8ab702876bc61aef907d85c
YuI2333 <yuilk@qq.com>
2026-08-09 05:10:29 +0800

    更新 sams_search_clean.js

--- Sams/sams_search_clean.js
+++ Sams/sams_search_clean.js
@@ -1,16 +1,17 @@
-// 提前拦截无响应体的请求，防止 Loon 抛出底层变量缺失错误
-if (typeof $response === "undefined" || !$response.body) {
-    $done({});
-} else {
+if (typeof $response !== "undefined" && $response.body) {
     try {
         let obj = JSON.parse($response.body);
         
         if (obj?.data?.trySearchKeywordResponse) {
-            delete obj.data.trySearchKeywordResponse;
+            // 不使用 delete 彻底删除节点，赋值为 null 或保留空层级
+            // 防止强类型语言解析数据模型时由于缺失字段而报错
+            obj.data.trySearchKeywordResponse = null; 
         }
         
         $done({ body: JSON.stringify(obj) });
     } catch (e) {
         $done({});
     }
+} else {
+    $done({});
 }
