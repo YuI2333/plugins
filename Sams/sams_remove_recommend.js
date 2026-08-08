30762296204f47ed188cc1692e28fbde89157efc
YuI2333 <yuilk@qq.com>
2026-08-09 02:02:59 +0800

    更新 sams_remove_recommend.js

--- Sams/sams_remove_recommend.js
+++ Sams/sams_remove_recommend.js
@@ -1,21 +1,11 @@
 try {
     let obj = JSON.parse($response.body);
-    
-    if (obj && obj.data) {
-        // 去除“为您推荐”商品列表
-        if (Array.isArray(obj.data.dataList)) {
-            obj.data.dataList = [];
-        }
-        // 去除“试试搜这些”推荐词
-        if (Array.isArray(obj.data.searchFilterList)) {
-            obj.data.searchFilterList = [];
-        }
-        if (Array.isArray(obj.data.cardFilterList)) {
-            obj.data.cardFilterList = [];
-        }
+    // 定位并清空推荐列表数据
+    if (obj && obj.data && obj.data.dataList) {
+        obj.data.dataList = [];
     }
-    
     $done({ body: JSON.stringify(obj) });
 } catch (e) {
+    // 发生异常时原样返回，避免应用网络请求卡死
     $done({});
 }
