91725ff299cc7836158fe4a33b7cb0431ad223f9
YuI2333 <yuilk@qq.com>
2026-08-09 15:53:22 +0800

    更新 sams_buytogo_remove.js

--- Sams/sams_buytogo_remove.js
+++ Sams/sams_buytogo_remove.js
@@ -2,9 +2,21 @@ try {
     if ($response && $response.body) {
         let obj = JSON.parse($response.body);
         
-        // 使用可选链 (?.) 安全判断并删除节点
-        if (obj?.data?.buyToGoInfo) {
-            delete obj.data.buyToGoInfo;
+        if (obj?.data) {
+            // 1. 去除“省心带一件”模块
+            if (obj.data.buyToGoInfo) {
+                delete obj.data.buyToGoInfo;
+            }
+            
+            // 2. 去除顶部“天气原因订单延迟”滚动提示
+            if (obj.data.bannerInfo) {
+                if (obj.data.bannerInfo.deliveryDelayTipsNewList) {
+                    obj.data.bannerInfo.deliveryDelayTipsNewList = [];
+                }
+                if (obj.data.bannerInfo.deliveryDelayTips) {
+                    obj.data.bannerInfo.deliveryDelayTips = "";
+                }
+            }
         }
         
         $done({ body: JSON.stringify(obj) });
@@ -12,6 +24,7 @@ try {
         $done({});
     }
 } catch (error) {
-    console.log("山姆去除省心带一件脚本运行异常: " + error);
+    console.log("山姆确认订单页净化脚本运行异常: " + error);
+    // 发生异常时静默退出，不影响 App 正常网络请求
     $done({});
 }
