506b8a41d70a652b5c0843088700539d47cdd697
YuI2333 <yuilk@qq.com>
2026-08-16 00:56:03 +0800

    创建 sams_paymentSuccessRecommend.js

---
+++ Sams/sams_paymentSuccessRecommend.js
@@ -0,0 +1,2 @@
+const mockBody = '{"rt":0,"success":true,"data":{"cardFilterList":[],"searchFilterList":[],"dataList":[],"recommendMap":{"recommendHasNextPage":false}}}';
+$done({ body: mockBody });
