2d90e9ac2aa6880dfed0f195e38b26b4d71f2c79
YuI2333 <yuilk@qq.com>
2026-09-12 21:51:14 +0800

    更新 sams_notice.js

--- Sams/sams_notice.js
+++ Sams/sams_notice.js
@@ -1,11 +1,19 @@
-if (!$response.body) $done({});
+let body = $response.body;
 
-try {
-    let obj = JSON.parse($response.body);
-    if (Array.isArray(obj.data)) {
-        obj.data = obj.data.filter(item => ![4, 5, 7].includes(item.currentType));
-    }
-    $done({ body: JSON.stringify(obj) });
-} catch (e) {
+if (!body) {
     $done({});
+} else {
+    try {
+        let obj = JSON.parse(body);
+        if (obj && Array.isArray(obj.data)) {
+            // 过滤掉类型为 4(会籍账户), 5(山姆活动), 7(互动消息) 的数据
+            obj.data = obj.data.filter(item => {
+                return item && ![4, 5, 7].includes(item.currentType);
+            });
+        }
+        $done({ body: JSON.stringify(obj) });
+    } catch (e) {
+        console.log("山姆通知脚本解析异常: " + e);
+        $done({});
+    }
 }
