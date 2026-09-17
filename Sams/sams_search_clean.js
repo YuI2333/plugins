79294b0f4300d53a22553db0200f2a11dddf4be1
YuI2333 <yuilk@qq.com>
2026-09-17 19:00:18 +0800

    更新 sams_search_clean.js

--- Sams/sams_search_clean.js
+++ Sams/sams_search_clean.js
@@ -1,11 +1,9 @@
-let body = $response.body;
-
-if (typeof body !== "undefined" && body !== null) {
+if (typeof $response !== "undefined" && $response.body) {
     try {
-        let obj = JSON.parse(body);
-        let modified = false;
+        var body = $response.body;
+        var obj = JSON.parse(body);
+        var modified = false;
 
-        // 弃用 ?. 语法，改用传统 && 链式判断，彻底解决 Line:0 编译报错
         if (
             obj.data &&
             obj.data.trySearchKeywordResponse &&
@@ -18,8 +16,7 @@ if (typeof body !== "undefined" && body !== null) {
         }
         
         if (modified) {
-            // 使用安全的正则写法过滤不可见段落分隔符，防止原生环境解析崩溃
-            let newBody = JSON.stringify(obj).replace(/[\u2028\u2029]/g, "");
+            var newBody = JSON.stringify(obj).replace(/[\u2028\u2029]/g, "");
             $done({ body: newBody });
         } else {
             $done({});
